f7bb263768e9606f541f3bd71c22d502bb384d01
ngtolstova <124882480+ngtolstova@users.noreply.github.com>
2023-03-04 00:17:16 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,44 @@
 
 
 double pown(double value, uint16_t n) {
-
+  double res = 1;
+  for (uint16_t i = 0; i < n; i++){
+  res *= value;
+  }
+  return res;
 }
 
 uint64_t fact(uint16_t n) {
-
+  if (n <= 1)
+    return 1;
+  else
+    return n * fact(n - 1);
 }
 
 double calcItem(double x, uint16_t n) {
-
+  return (pown(x, n) / fact(n));
 }
 
 double expn(double x, uint16_t count) {
-
+  double sum = 0;
+  for (uint16_t n = 0; n < count; n++){
+    sum += (pown(x, n) / fact(n));
+  }
+  return sum;
 }
 
 double sinn(double x, uint16_t count) {
-
+  double sum = 0;
+  for (uint16_t n = 0; n < count; n++){
+    sum += pown(-1, n - 1) * (pown(x, 2 * n - 1) / fact(2 * n - 1))
+  }
+  return sum;
 }
 
 double cosn(double x, uint16_t count) {
-
+  double sum = 0;
+  for (uint16_t n = 0; n < count; n++){
+    sum += pown(-1, n - 1) * (pown(x, 2 * n - 2) / fact(2 * n - 2))
+  }
+  return sum;
 }
